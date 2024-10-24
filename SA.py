d45c4f86b49751d32eb754ae5e9d88ed87fa13c4
ouoxii <yuchuner317@gmail.com>
2024-10-24 10:30:22 +0800

    bulb change

--- SA.py
+++ SA.py
@@ -1,11 +1,11 @@
-import random 
+import random
 
-ServerURL = 'URL' #For example: 'https://DomainName'
-MQTT_broker = None # MQTT Broker address, for example: 'DomainName' or None = no MQTT support
-MQTT_port = 1883
-MQTT_encryption = False
-MQTT_User = 'ID'
-MQTT_PW = 'PW'
+ServerURL = 'https://class.iottalk.tw' #For example: 'https://DomainName'
+MQTT_broker = 'iot.iottalk.tw' # MQTT Broker address, for example: 'DomainName' or None = no MQTT support
+MQTT_port = 8883
+MQTT_encryption = True
+MQTT_User = 'iottalk'
+MQTT_PW = 'iottalk2023'
 
 device_model = 'Dummy_Device'
 IDF_list = ['Dummy_Sensor']
@@ -14,20 +14,35 @@ device_id = None #if None, device_id = MAC address
 device_name = None
 exec_interval = 1  # IDF/ODF interval
 
+# 設定兩個全域變數用來追蹤遞增和遞減的數值
+increasing = True  # 用來控制是遞增還是遞減
+value = 0  # 初始值
+
 def Dummy_Sensor():
-    return random.randint(0, 100), random.randint(0, 100) 
+    global value, increasing
+
+    if increasing:
+        value += 10
+        if value >= 100:  # 當達到 100 時開始遞減
+            increasing = False
+    else:
+        value -= 10
+        if value <= 0:  # 當降到 0 時開始遞增
+            increasing = True
+
+    return value, value
 
 def Dummy_Control(data:list):
     print(data[0])
 
 def on_register(r):
-    print(f'Device name: {r["d_name"]}')    
+    print(f'Device name: {r["d_name"]}')
     '''
-    #You can write some SA routine code here, for example: 
+    #You can write some SA routine code here, for example:
     import time, DAI
     while True:
-        DAI.push('Dummy_Sensor', [100, 200])  
-        time.sleep(exec_interval)    
+        DAI.push('Dummy_Sensor', [100, 200])
+        time.sleep(exec_interval)
     '''
 
 
